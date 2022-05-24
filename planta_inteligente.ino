28da59aa7644628d445c01dea3414c7cc7b0f9c6
Nubiabrune <99697649+Nubiabrune@users.noreply.github.com>
2022-05-23 21:27:16 -0300

    Add files via upload

---
+++ planta_inteligente.ino
@@ -0,0 +1,147 @@
+// ===================
+// Incluir bibliotecas
+// ===================
+#include <ESP8266WiFi.h> //inclui suporte ao NodeMCU
+#include <PubSubClient.h> //inclui suporte ao MQTT no HiveMQ Cloud
+
+// =======================
+// Definicoes e constantes
+// =======================
+
+// Configuracoes da rede WiFi
+const char* ssid = "VIVO HDS"; //SSID da rede WiFi
+const char* password = "henumafest"; //senha da rede WiFi
+
+const char* mqtt_server = "broker.mqtt-dashboard.com"; //URL do broker MQTT
+const int mqtt_server_port = 1883; //porta do broker MQTT
+
+// Variaveis globais e objetos
+#define MSG_BUFFER_SIZE (500) //define MSG_BUFFER_SIZE como 500
+WiFiClient client; //cria o objeto client
+PubSubClient mqtt_client(client); //cria o objeto mqtt_client
+long lastMsg = 0;
+
+String clientID = "ESP8266Client-"; //identificacao do cliente
+
+String topicoPrefixo = "MACK20009161"; //para o prefixo do topico, utilizar MACK seguido do TIA
+String topicoTodos = topicoPrefixo + "/#"; //para retornar todos os topicos
+String topico_0 = topicoPrefixo + "/hello"; //topico para teste
+String mensagem_0 = "NodeMCU Connected"; //mensagem para o topico 0
+String topico_1 = topicoPrefixo + "/sensor1"; //topico para o sensor 1
+String mensagem_1 = ""; //mensagem para o topico 1
+String topico_2 = topicoPrefixo + "/atuador1"; //topico para o atuador 1
+String mensagem_2 = ""; //mensagem para o topico 2
+String mensagemTemp = ""; //armazena temporariamente as mensagens recebidas via MQTT
+
+
+// ==============================
+// Funcoes definidas pelo usuario
+// ==============================
+
+// Funcao para configurar a conexao WiFi
+void setup_wifi() {
+  //WiFi.mode(WIFI_STA); //ativa o modo de estacao
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Funcao para receber mensagens
+void callback(char* topic, byte* payload, unsigned int length) {
+  mensagemTemp = "";
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    mensagemTemp += (char)payload[i];
+  }
+
+  if (String(topic) == topico_2) {
+    mensagem_2 = mensagemTemp;
+  }
+
+  Serial.println();
+
+ }
+
+// Funcao para conectar no broker MQTT
+void reconnect() {
+  // Loop until we’re reconnected
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection…");
+
+	// Create a random client ID
+    randomSeed(micros()); //inicializa a semente do gerador de numeros aleatorios
+    clientID += String(random(0xffff), HEX);
+
+	// Attempt to connect
+    if (mqtt_client.connect(clientID.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqtt_client.publish(topico_0.c_str(), mensagem_0.c_str());
+      // ... and resubscribe
+      mqtt_client.subscribe(topicoTodos.c_str());
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}	
+
+// =======
+// setup()
+// =======
+#define rele D3
+#define sensor D2
+
+bool irrigar = false;
+void setup() {
+  pinMode(rele, INPUT); //pino zero, onde esta ligado o pushbutton
+  pinMode(sensor, OUTPUT); //pino 1, onde esta ligado o LED
+ 
+  digitalWrite (rele, HIGH); 
+  Serial.begin(9600);
+  setup_wifi();
+
+  mqtt_client.setServer(mqtt_server, mqtt_server_port); //configura a conexao com o broker MQTT
+  mqtt_client.setCallback(callback); //configura o recebimento de mensagens
+}
+
+// ======
+// loop()
+// ======
+
+void loop() {
+  if (!mqtt_client.connected()) {
+    reconnect();
+  }
+  mqtt_client.loop(); //processa as mensagens e mantem a conexao com o broker MQTT
+
+  irrigar = digitalRead(sensor);
+
+  if (irrigar)
+  {
+    digitalWrite(rele, LOW);
+  }
+  else
+  {
+    digitalWrite(rele, HIGH);
+  }
+
+  delay(500);
+}
+  
+ 
